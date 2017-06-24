f44a9fe14afe73c0d93b265b0e6a442755689e10
Kristen Thyng <kthyng@gmail.com>
2017-06-23 23:30:09 -0500

    plots is now working for intime and mean

--- plots.py
+++ plots.py
@@ -1,7 +1,14 @@
 '''
 Plotting.
 Example:
-python plots.py 'vort' 'full'
+python2 plots.py 'vort' 'full' 'bar' 'intime'
+python2 plots.py 'mix' 'full' 'bar' 'intime'
+python2 plots.py 'rho' 'full' 'surface' 'intime'
+python2 plots.py 'upwelling' 'full' 'bar' 'intime'
+python2 plots.py 'upsloping' 'full' 'bar' 'intime'
+python2 plots.py 'mix' 'full' 'bar' 'mean'
+python2 plots.py 'upwelling' 'full' 'bar' 'mean'
+python2 plots.py 'upsloping' 'full' 'bar' 'mean'
 '''
 
 import xarray as xr
@@ -22,6 +29,8 @@ mpl.rcParams.update({'font.size': 10})
 import scipy.io
 import os
 import matplotlib.dates as Dates
+import octant
+import octant.depths
 
 
 merc = ccrs.Mercator()
@@ -32,20 +41,12 @@ grid = netCDF.Dataset(locgrid)
 pm = grid['pm'][:]; pn = grid['pn'][:]
 lon_psi = grid['lon_psi'][:]; lat_psi = grid['lat_psi'][:]
 lon_rho = grid['lon_rho'][:]; lat_rho = grid['lat_rho'][:]
-land_10m = cfeature.NaturalEarthFeature('physical', 'land', '10m',
-                                        edgecolor='face',
-                                        facecolor=cfeature.COLORS['land'])
-
-# model tidal signal for later
-# read in saved zeta
-# mat = scipy.io.loadmat('savedoutput/zeta_adcpzetasites.mat')
-# location 0 is in the middle of the channel between Admiralty Head and Point Wilson
 dates = pd.date_range("2006-09-01", "2006-10-01", freq="15T")
-# df = pd.DataFrame(index=dates)
-# df['zeta'] = mat['data'][:,0]
-# zeta = mat['data'][:,0]
-# zmax = max(abs(zeta))
-
+h = grid['h'][:]
+ind = (lon_rho > -122.64)*(lat_rho > 48.17)
+h[ind] = np.nan
+ind = (lon_rho > -122.58)*(lat_rho > 48.0)
+h[ind] = np.nan
 def get_zeta():
     '''Read in zeta time series for plotting.'''
     # lon_zeta = -122.71497166666667; lat_zeta = 48.149936666666669
@@ -80,32 +81,21 @@ def re(A, dim):
     # B is A but rolled such that the dimension that is to be resized is in
     # the 0 position
     B = np.rollaxis(A, dim)
-
     # Do averaging
     B = 0.5*(B[0:-1]+B[1:])
-
     # Roll back to original
     return np.rollaxis(B, 0, dim+1)
 
 
-def setup(zoom='full'):
+def setup(zoom='full', dobathy=False):
     '''Setup plot.'''
 
     if zoom == 'full':
         figsize = 5.25, 7
         box = [-122.8, -122.54, 47.9655, 48.227]
     fig = plt.figure(figsize=figsize)
-    fig.subplots_adjust(top=0.96, bottom=0.04, left=0.08, right=0.99)
+    fig.subplots_adjust(top=0.96, bottom=0.02, left=0.06, right=0.99)
     ax = fig.add_subplot(111, projection=merc)#, facecolor='0.8')
-    # ax.set_facecolor("0.8")
-    # ax.background_patch.set_fill(True)
-    # Set RGB value to land color
-    # ax.imshow(np.tile(np.array([[[235, 235, 235]]],
-    #           dtype=np.uint8), [2, 2, 1]),
-    #       origin='center',
-    #       transform=pc,
-    #       extent=box)
-
     ax.set_extent(box, pc)
     gl = ax.gridlines(linewidth=0.2, color='gray', alpha=0.5, linestyle='-', draw_labels=True)
     gl.xlocator = mticker.FixedLocator([-122.8, -122.75, -122.7, -122.65, -122.6, -122.55, -122.5])
@@ -114,17 +104,16 @@ def setup(zoom='full'):
     gl.yformatter = LATITUDE_FORMATTER
     gl.xlabels_top = False  # turn off labels where you don't want them
     gl.ylabels_right = False
-
-    # plot isobaths
-    # ax.contour(grid['lon_rho'][:], grid['lat_rho'][:], grid['h'][:], np.arange(0, 200, 20), colors='0.6', transform=pc, linewidths=0.5)
-
     # plot coastline
-    ax.contour(grid['lon_rho'][:], grid['lat_rho'][:], grid['mask_rho'][:], [0], colors='0.3', transform=pc, linewidths=2, alpha=0.7)
+    ax.contour(grid['lon_rho'][:], grid['lat_rho'][:], grid['mask_rho'][:], [0], colors='0.3', transform=pc, linewidths=1.5)
+    # plot bathymetry
+    if dobathy:
+        ax.contour(grid['lon_rho'][:], grid['lat_rho'][:], h, np.arange(25,200,25), colors='0.3', transform=pc, linewidths=0.25)
 
     return fig, ax
 
 
-def setupvar(varname):
+def setupvar(varname, kind):
     '''Setup variable and other choices given the variable plotted.'''
 
     if varname == 'vort':
@@ -133,13 +122,61 @@ def setupvar(varname):
         vmin = -vmax
         lon = lon_rho
         lat = lat_rho
-        label = r'Vertical vorticity [s$^{-1}$]'
+        label = 'Vertical\nvorticity\n[s$^{-1}$]'
         ticks = np.linspace(vmin, vmax, 5)
         ctidal = '#86174F' ## pink or '#107469' teal
+        alpha = 1
+    elif varname == 'mix':
+        cmap = cmo.amp
+        if kind == 'intime':
+            vmax = 0.5
+        elif kind == 'mean':
+            vmax = 0.1
+        vmin = 0
+        lon = lon_psi
+        lat = lat_psi
+        label = 'Vertical\nmixing\n[?]'
+        ticks = np.linspace(vmin, vmax, 6)
+        ctidal = '#71001D'
+        alpha = 0.6
+    elif varname == 'rho':
+        cmap = cmo.haline
+        vmax = 24.5
+        vmin = 21
+        lon = lon_psi
+        lat = lat_psi
+        label = 'Density\n[kg$\cdot$ m$^{-3}$]'
+        ticks = np.linspace(vmin, vmax, 6)
+        ctidal = '#0A3885'
+        alpha = 0.6
+    elif varname == 'upwelling':
+        cmap = cmo.delta
+        if kind == 'intime':
+            vmax = 0.1
+        elif kind == 'mean':
+            vmax = 0.05
+        vmin = -vmax
+        lon = lon_psi
+        lat = lat_psi
+        label = 'Upwelling\nvelocity\n[m$\cdot$s$^{-1}$]'
+        ticks = np.linspace(vmin, vmax, 6)
+        ctidal = '#226E13'
+        alpha = 0.6
+    elif varname == 'upsloping':
+        cmap = cmo.delta
+        if kind == 'intime':
+            vmax = 0.1
+        elif kind == 'mean':
+            vmax = 0.05
+        vmin = -vmax
+        lon = lon_psi
+        lat = lat_psi
+        label = 'Upsloping\nvelocity\n[m$\cdot$s$^{-1}$]'
+        ticks = np.linspace(vmin, vmax, 6)
+        ctidal = '#226E13'
+        alpha = 0.6
 
-    # elif varname == 'salt':
-
-    return lon, lat, cmap, vmin, vmax, label, ticks, ctidal
+    return lon, lat, cmap, vmin, vmax, label, ticks, ctidal, alpha
 
 
 def setupvarforstep(m, varname, depth='bar'):
@@ -153,36 +190,49 @@ def setupvarforstep(m, varname, depth='bar'):
             v = m['v'][-1,:,:]
         var = (v[:,:-1] - v[:,1:])*re(re(pm,1),0) - (u[:-1,:] - u[1:,:])*re(re(pn,1),0)
 
-    # elif varname == 'mix'
-    # elif varname == 'salt':
+    elif varname == 'mix':
+        if depth == 'bar':
+            AKs = m['AKs'][0, 1:-1, 1:-1, 1:-1].squeeze()  # vertical viscosity for scalar
+            rho = m['rho'][0, :, 1:-1, 1:-1].squeeze()  # salt
+            zr = octant.depths.get_zrho(m['Vtransform'][0], m['Vstretching'][0],
+                                       m['u'].shape[1], m['theta_s'][0]+0.0000001,
+                                       m['theta_b'][0], m['h'][:], m['hc'][:], zeta=m['zeta'][0],
+                                       Hscale=3)
+            dz = np.diff(zr[:,1:-1,1:-1], axis=0)
+            dsdz = (rho[:-1] - rho[1:])*dz
+            var = (AKs*dsdz).sum(axis=0)
+
+    elif varname == 'rho':
         # if depth == 'bar':
-        #     v = m['v'].sel(ocean_time=date)
-        # elif depth == 'surface':
-        #     v = m['v'].sel(ocean_time=date).isel(s_rho=-1, eta_rho=slice(1,-1), xi_rho=slice(1,-1))
-    # Note: skip ghost cells in x and y so that can properly plot grid cell boxes with pcolormesh
-    # salt = m.salt.sel(ocean_time=plotdate).isel(s_rho=-1, eta_rho=slice(1,-1), xi_rho=slice(1,-1))
+        #     v = m['vbar'][:].squeeze()
+        #     u = m['ubar'][:].squeeze()
+        if depth == 'surface':
+            # Note: skip ghost cells in x and y so that can properly plot grid cell boxes with pcolormesh
+            var = m['rho'][0,-1,1:-1,1:-1].squeeze()
+
+    elif varname == 'upwelling':
+        if depth == 'bar':
+            var = m['omega'][0,:,1:-1,1:-1].squeeze().mean(axis=0)
+        # if depth == 'surface':
+        #     # Note: skip ghost cells in x and y so that can properly plot grid cell boxes with pcolormesh
+        #     var = m['rho'][0,-1,1:-1,1:-1].squeeze()
+
+    elif varname == 'upsloping':
+        if depth == 'bar':
+            w = m['w'][0,:,1:-1,1:-1].squeeze()
+            uw = m['omega'][0,:,1:-1,1:-1].squeeze()
+            var = (w - uw).mean(axis=0)
+        # if depth == 'surface':
+        #     # Note: skip ghost cells in x and y so that can properly plot grid cell boxes with pcolormesh
+        #     var = m['rho'][0,-1,1:-1,1:-1].squeeze()
 
     return var
 
 
-def add_stuff(lon, lat, vart, cmap, vmin, vmax, label, ticks):
+def add_stuff(lon, lat, vart, cmap, vmin, vmax, label, ticks, alpha):
     '''Plot actual stuff.'''
 
     mapp = ax.pcolormesh(lon, lat, vart, cmap=cmap, vmin=vmin, vmax=vmax, transform=pc)
-    # ax.add_feature(land_10m, facecolor='0.8')
-    # ax.coastlines(resolution='10m')  # coastline resolution options are '110m', '50m', '10m'
-    # land data from: http://openstreetmapdata.com/data/land-polygons
-    # example from: https://ocefpaf.github.io/python4oceanographers/blog/2015/06/22/osm/
-    # shp = shapereader.Reader('data/land-polygons-complete-4326/land_polygons')
-    # for record, geometry in zip(shp.records(), shp.geometries()):
-    #     ax.add_geometries([geometry], pc, facecolor='0.8', edgecolor='black')
-    # # coastline
-    # shp = shapereader.Reader('./data/OSM_coastline/BTS')
-    # for record, geometry in zip(shp.records(), shp.geometries()):
-    #     ax.add_geometries([geometry], ccrs.PlateCarree(), facecolor='w',
-    #                       edgecolor='black')
-
-    ax.set_title(label)
 
     # Colorbar in lower left corner
     cax = fig.add_axes([0.795, 0.25, 0.02, 0.35]) #colorbar axes
@@ -194,58 +244,49 @@ def add_stuff(lon, lat, vart, cmap, vmin, vmax, label, ticks):
     # change colorbar tick color http://stackoverflow.com/questions/9662995/matplotlib-change-title-and-colorbar-text-and-tick-colors
     cbtick = plt.getp(cb.ax.axes, 'yticklabels')
     plt.setp(cbtick, color='0.2')
+    # text above colorbar
+    cax.text(1.5, 1.1, label, ha='center', fontsize=12, transform=cax.transAxes)
 
 
 def add_tidal(date, ctidal):
     '''Add tidal signal.'''
 
     axt = fig.add_axes([0.43, 0.85, 0.5, .11], facecolor='None')
-    # axt.background_patch.set_fill(False)
     # turn frame off
     for axis in ['top','left','right','bottom']:
         axt.spines[axis].set_linewidth(0.0)
-        # axr.spines[axis].set_linewidth(0.05)
-    # axr.spines['bottom'].set_linewidth(0.0)
-    # ticks
-
-    # axt.yaxis.tick_right()
+    # control y ticks
     axt.set_yticks([-1.5, 0])  # max is skewed off top of plot
     axt.tick_params(axis='y', pad=0, labelsize=8, length=0, color='0.5', labelcolor='0.5')
-
-    # axt.get_yaxis().set_visible(False)
-    # axt.get_xaxis().set_visible(False)
-    df.plot(ax=axt, color='0.5', lw=0.9, legend=False, rot=-30)
-    axt.plot(date, df[date:date], 'o', ms=5, color=ctidal)
+    # plot tide signal
+    df.plot(ax=axt, color='0.5', lw=1, legend=False, rot=-30)
+    df[date:date].plot(marker='o', ax=axt, ms=4, color=ctidal, legend=False)
     axt.xaxis.set_major_locator(Dates.DayLocator())
     axt.xaxis.set_major_formatter(Dates.DateFormatter('%d'))
-    axt.text(0.55, 0.8, 'September 2006', transform=ax.transAxes)
-    # # the plot itself, in indices
-    # axt.plot(np.arange(zeta.size), zeta, '0.5', lw=0.9)
-    # axt.plot(itime, zeta[itime], 'o', ms=5, color=ctidal)
+    axt.text(0.55, 0.8, 'September 2006', transform=ax.transAxes, color='0')
+    fig.text(0.12, 0.025, date, transform=ax.transAxes, color='0', fontsize=12)
 
 
 def add_arrows(ax, m, depth='bar', zoom='full'):
     '''add current quiver arrows.'''
 
     if zoom == 'full':
-        ddx = 12; ddy = 12
-        qkx, qky = 0.17, 0.04  # quiver key location
+        ddx = 8; ddy = 8
+        qkx, qky = 0.15, 0.08  # quiver key location
 
     if depth == 'bar':
         v = m['vbar'][:].squeeze()
         u = m['ubar'][:].squeeze()
     elif depth == 'surface':
-        v = m['v'][-1,:,:]
-        u = m['u'][-1,:,:]
+        v = m['v'][0,-1,:,:].squeeze()
+        u = m['u'][0,-1,:,:].squeeze()
     u, v = re(u,0), re(v,1)  # resize to psi grid
 
     Q = ax.quiver(lon_psi[::ddy, ::ddx], lat_psi[::ddy, ::ddx],
-                  u[::ddy, ::ddx], v[::ddy, ::ddx], color='k', alpha=0.4,
-                  pivot='middle', scale=20, transform=pc)
-    qk = ax.quiverkey(Q, qkx, qky, 0.5, r'0.5 m$\cdot$s$^{-1}$ current',
+                  u[::ddy, ::ddx], v[::ddy, ::ddx], color='k', alpha=0.3,
+                  pivot='middle', scale=25, transform=pc)
+    qk = ax.quiverkey(Q, qkx, qky, 1.0, r'1.0 m$\cdot$s$^{-1}$ current',
                       labelcolor='0.5', fontproperties={'size': '10'})
-
-
 
 
 if __name__ == "__main__":
@@ -255,25 +296,66 @@ if __name__ == "__main__":
     parser.add_argument('var', type=str, help='variable to plot', default="salt")
     parser.add_argument('zoom', type=str, help='which zoom to use: "full"', default="full")
     parser.add_argument('depth', type=str, help='which depth: "bar", "surface"', default="bar")
+    parser.add_argument('kind', type=str, help='which kind over time: "intime", "mean" across 30 days', default="intime")
     # parser.add_argument('--dend', type=str, help='dend', default=None)
     args = parser.parse_args()
     varname = args.var
     zoom = args.zoom
     depth = args.depth
+    kind = args.kind
 
     # prep variable
-    lon, lat, cmap, vmin, vmax, label, ticks, ctidal = setupvar(varname)
-
+    lon, lat, cmap, vmin, vmax, label, ticks, ctidal, alpha = setupvar(varname, kind)
+    # tidal signal
     df = get_zeta()
 
-    fig, ax = setup(zoom=zoom)
-    # for i, date in zip(range(2881), dates):
-    i=1000; date = pd.to_datetime(dates[i]).strftime('%Y-%m-%02d %H:%M')
-    m = netCDF.Dataset(locmodel + 'ocean_his_' + str(i+1).zfill(4) + '.nc')
-    # dates = m['ocean_time'].data
-    vart = setupvarforstep(m, varname, depth=depth)
-    add_stuff(lon, lat, vart, cmap, vmin, vmax, label, ticks)
-    add_tidal(date, ctidal) # need to save a tidal signal to use
-    add_arrows(ax, m, depth=depth, zoom=zoom)
-    fig.savefig('temp.png', bbox_inches='tight')
-        # import pdb; pdb.set_trace()
+    # make directories
+    basename = 'figures/' + varname + '/' + zoom + '/' + depth + '/'
+    basenames = basename.split('/')
+    for i in range(len(basenames)-1):
+        tempname = '/'.join(basenames[:i+1])
+        if not os.path.exists(tempname):
+            os.makedirs(tempname)
+
+    if varname == 'mix' or varname == 'upwelling' or varname == 'upsloping':
+        dobathy = True
+    else:
+        dobathy = False
+
+    ntimes = len(dates)
+
+    if kind == 'mean':
+        Var = np.zeros((lon.shape[0]-1, lon.shape[1]-1))
+    for i in range(ntimes):
+        if kind == 'intime':
+            date = pd.to_datetime(dates[i]).strftime('%b %02d, %Y %H:%M')
+            fname = basename + pd.to_datetime(dates[i]).strftime('%Y-%m-%02dT%H:%M')
+        elif kind == 'mean':
+            fname = basename + 'mean'
+        if os.path.exists(fname + '.png'):
+            continue
+
+        m = netCDF.Dataset(locmodel + 'ocean_his_' + str(i+1).zfill(4) + '.nc')
+        vart = setupvarforstep(m, varname, depth=depth)
+        if kind == 'mean':
+            meanname = 'savedoutput/' + varname + '_mean.npz'
+            if os.path.exists(meanname):
+                i = ntimes-1  # short-circuit loop and jump to end
+                meanfile = np.load(meanname)
+                vart = meanfile['var'].item()
+            else:
+                Var += vart
+                # on the last time step, complete average and rename
+                if i == ntimes-1:
+                    vart = Var/ntimes
+                    np.savez(meanname, lon=lon, lat=lat, var=vart)
+                else:
+                    continue
+
+        fig, ax = setup(zoom=zoom, dobathy=dobathy)
+        add_stuff(lon, lat, vart, cmap, vmin, vmax, label, ticks, alpha)
+        if kind == 'intime':
+            add_tidal(date, ctidal) # need to save a tidal signal to use
+            add_arrows(ax, m, depth=depth, zoom=zoom)
+        fig.savefig(fname + '.png', bbox_inches='tight', dpi=100)
+        plt.close(fig)
