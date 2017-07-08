366ead2dd5f532fe0404e4855dbe86e332f1fd4a
Kristen Thyng <kthyng@gmail.com>
2017-07-08 10:52:53 -0500

    about to change calcs to try to be more precise

--- calcs.py
+++ calcs.py
@@ -22,9 +22,9 @@ import os
 import matplotlib.dates as Dates
 from plots import re
 
-which = 'all' # 'all' 'hor'
+which = 'testtop' # 'all' 'center'
 kind = 'barotropic'
-doterms = False
+doterms = True
 
 locgrid = '/pong/raid/kthyng/froude/ai65/grid.nc'
 locmodel = '/pong/raid/kthyng/froude/ai65/OUT/'
@@ -41,31 +41,76 @@ Cd = 3e-3  # friction coefficient
 rho0 = 1023.7
 
 
-if which == 'hor':
+# if which == 'hor':
+#     # west transects: [0:1, 2]
+#     twest = np.array([[245, 340, 65], [0, 245, 160]])
+#     # east transects: [0:1, 2]
+#     teast = np.array([[310, 340, 120], [0, 310, 200]])
+#     # north transects: [0, 1:2]
+#     tnorth = np.array([[340, 65, 120], [310,120,200]])
+#     # south transects: [0, 1:2]
+#     tsouth = np.array([[245, 65, 160]])
+if which == 'center':
     # west transects: [0:1, 2]
-    twest = np.array([[245, 340, 65], [0, 245, 160]])
+    twest = np.array([[5, 310, 160]])
     # east transects: [0:1, 2]
-    teast = np.array([[310, 340, 120], [0, 310, 200]])
+    teast = np.array([[5, 310, 200]])
     # north transects: [0, 1:2]
-    tnorth = np.array([[340, 65, 120], [310,120,200]])
+    tnorth = np.array([[310,160,200]])
     # south transects: [0, 1:2]
-    tsouth = np.array([[245, 65, 160]])
+    tsouth = np.array([[5, 160,200]])
+elif which == 'testtop':
+    # west transects: [0:1, 2]
+    twest = np.array([[335, 375, 5]])
+    # east transects: [0:1, 2]
+    teast = np.array([[335, 375, 280]])
+    # north transects: [0, 1:2]
+    tnorth = np.array([[375,5,280]])
+    # south transects: [0, 1:2]
+    tsouth = np.array([[335, 5,280]])
+elif which == 'testbottom':
+    # west transects: [0:1, 2]
+    twest = np.array([[295, 335, 5]])
+    # east transects: [0:1, 2]
+    teast = np.array([[295, 335, 280]])
+    # north transects: [0, 1:2]
+    tnorth = np.array([[335,5,280]])
+    # south transects: [0, 1:2]
+    tsouth = np.array([[295, 5,280]])
+elif which == 'centersmall':
+    # west transects: [0:1, 2]
+    twest = np.array([[200, 310, 160]])
+    # east transects: [0:1, 2]
+    teast = np.array([[200, 310, 200]])
+    # north transects: [0, 1:2]
+    tnorth = np.array([[310,160,200]])
+    # south transects: [0, 1:2]
+    tsouth = np.array([[200, 160,200]])
+elif which == 'centersmallwwall':
+    # west transects: [0:1, 2]
+    twest = np.array([[200, 340, 160]])
+    # east transects: [0:1, 2]
+    teast = np.array([[200, 340, 200]])
+    # north transects: [0, 1:2]
+    tnorth = np.array([[340,160,200]])
+    # south transects: [0, 1:2]
+    tsouth = np.array([[200, 160,200]])
 elif which == 'all':
     twest = np.array([[296, 442, 5]])
     teast = []
     tnorth = np.array([[440, 5, 38]])
     tsouth = np.array([[5, 130, 295]])
 
-PEeast = np.empty(ntimes); KEeast = np.empty(ntimes)
-PEwest = np.empty(ntimes); KEwest = np.empty(ntimes)
-PEnorth = np.empty(ntimes); KEnorth = np.empty(ntimes)
-PEsouth = np.empty(ntimes); KEsouth = np.empty(ntimes)
-Pin = np.empty(ntimes); Pout = np.empty(ntimes)
-Pwest = np.empty(ntimes); Pnorth = np.empty(ntimes)
-Psouth = np.empty(ntimes); Peast = np.empty(ntimes)
+PEeast = np.zeros(ntimes); KEeast = np.zeros(ntimes)
+PEwest = np.zeros(ntimes); KEwest = np.zeros(ntimes)
+PEnorth = np.zeros(ntimes); KEnorth = np.zeros(ntimes)
+PEsouth = np.zeros(ntimes); KEsouth = np.zeros(ntimes)
+Pin = np.zeros(ntimes); Pout = np.zeros(ntimes)
+Pwest = np.zeros(ntimes); Pnorth = np.zeros(ntimes)
+Psouth = np.zeros(ntimes); Peast = np.zeros(ntimes)
 if doterms:
-    Pmix = np.empty(ntimes); Pfriction = np.empty(ntimes)
-    Pmom = np.empty(ntimes)
+    Pmix = np.zeros(ntimes); Pfriction = np.zeros(ntimes)
+    Pmom = np.zeros(ntimes)
 for i in range(ntimes):
 
     m = netCDF.Dataset(locmodel + 'ocean_his_' + str(i+1).zfill(4) + '.nc')
@@ -81,6 +126,8 @@ for i in range(ntimes):
         Hwest = (h[vec[0]:vec[1],vec[2]] + zetawest)  # height
         # PEwest[i] = rho0 * g * dy * (Hwest**2 * ubarwest).sum()  # potential energy, sum in y
         PEwest[i] = rho0 * g * dy * (Hwest * ubarwest * zetawest).sum()  # potential energy anomaly, sum in y
+        # PEwest[i] = 0.5 * rho0 * g * dy * (ubarwest * zetawest**2).sum()  # potential energy anomaly, sum in y
+        # PEwest[i] = 0.5 * rho0 * g * dy * (ubarwest * Hwest**2).sum()  # potential energy anomaly, sum in y
         if kind == 'barotropic':
             KEwest[i] = 0.5 * rho0 * dy * (Hwest * ubarwest * (ubarwest**2 + vbarwest**2)).sum()  # sum in y
         elif kind == 'baroclinic':
@@ -99,6 +146,8 @@ for i in range(ntimes):
         Heast = (h[vec[0]:vec[1],vec[2]] + zetaeast)  # height
         # PEeast[i] = rho0 * g * dy * (Heast**2 * ubareast).sum()  # potential energy, sum in y
         PEeast[i] = rho0 * g * dy * (Heast * ubareast * zetaeast).sum()  # potential energy anomaly, sum in y
+        # PEeast[i] = 0.5 * rho0 * g * dy * (ubareast * zetaeast**2).sum()  # potential energy anomaly, sum in y
+        # PEeast[i] = 0.5 * rho0 * g * dy * (ubareast * Heast**2).sum()  # potential energy anomaly, sum in y
         if kind == 'barotropic':
             KEeast[i] = 0.5 * rho0 * dy * (Heast * ubareast * (ubareast**2 + vbareast**2)).sum()  # sum in y
         elif kind == 'baroclinic':
@@ -115,8 +164,10 @@ for i in range(ntimes):
             vnorth = re(m['v'][0,:, vec[0]:vec[0]+2,vec[1]:vec[2]],1).squeeze()
         zetanorth = m['zeta'][0,vec[0],vec[1]:vec[2]]
         Hnorth = (h[vec[0],vec[1]:vec[2]] + zetanorth)  # height
-        # PEnorth[i] = rho0 * g * dy * (Hnorth**2 * vbarnorth).sum()  # potential energy, sum in y
+        # PEnorth[i] = rho0 * g * dx * (Hnorth**2 * vbarnorth).sum()  # potential energy, sum in y
         PEnorth[i] = rho0 * g * dx * (Hnorth * vbarnorth * zetanorth).sum()  # potential energy anomaly, sum in y
+        # PEnorth[i] = 0.5 * rho0 * g * dx * (vbarnorth * zetanorth**2).sum()  # potential energy anomaly, sum in y
+        # PEnorth[i] = 0.5 * rho0 * g * dx * (vbarnorth * Hnorth**2).sum()  # potential energy anomaly, sum in y
         if kind == 'barotropic':
             KEnorth[i] = 0.5 * rho0 * dx * (Hnorth * vbarnorth * (ubarnorth**2 + vbarnorth**2)).sum()  # sum in y
         elif kind == 'baroclinic':
@@ -135,6 +186,8 @@ for i in range(ntimes):
         Hsouth = (h[vec[0],vec[1]:vec[2]] + zetasouth)  # height
         # PEsouth[i] = rho0 * g * dy * (Hsouth**2 * vbarsouth).sum()  # potential energy, sum in y
         PEsouth[i] = rho0 * g * dx * (Hsouth * vbarsouth * zetasouth).sum()  # potential energy anomaly, sum in y
+        # PEsouth[i] = 0.5 * rho0 * g * dx * (vbarsouth * zetasouth**2).sum()  # potential energy anomaly, sum in y
+        # PEsouth[i] = 0.5 * rho0 * g * dx * (vbarsouth * Hsouth**2).sum()  # potential energy anomaly, sum in y
         if kind == 'barotropic':
             KEsouth[i] = 0.5 * rho0 * dx * (Hsouth * vbarsouth * (ubarsouth**2 + vbarsouth**2)).sum()  # sum in y
         elif kind == 'baroclinic':
@@ -142,29 +195,47 @@ for i in range(ntimes):
             KEsouth[i] = 0.5 * rho0 * dx * (dzsouth * vsouth * (usouth**2 + vsouth**2)).sum()  # sum in z and y
         Psouth[i] = PEsouth[i] + KEsouth[i]
 
-
+    # import pdb; pdb.set_trace()
     if doterms:
 
+        if teast == []:
+            teast = np.array([[0,0,-5]])
+        # if tsouth == []:
+        #     tsouth = np.array([[0,0,0]])
         ## Calculate mixing across inlet in time (buoyancy production)
-        AKs = m['AKs'][0, 1:-1, 5:-4, 5:-4].squeeze()  # vertical viscosity for tracer
-        rho = m['rho'][0, :, 5:-4, 5:-4].squeeze()  # density
-        zeta = m['zeta'][0,5:-4,5:-4]
-        dz = (h[5:-4,5:-4] + zeta)/20.  # easy since layers are uniform
+        AKs = m['AKs'][0, 1:-1, tsouth[0][0]:tnorth[0][0]+2, twest[0][2]:teast[0][2]+1].squeeze()  # vertical viscosity for tracer
+        rho = m['rho'][0, :, tsouth[0][0]:tnorth[0][0]+2, twest[0][2]:teast[0][2]+1].squeeze()  # density
+        zeta = m['zeta'][0,tsouth[0][0]:tnorth[0][0]+2, twest[0][2]:teast[0][2]+1]
+        dz = (h[tsouth[0][0]:tnorth[0][0]+2, twest[0][2]:teast[0][2]+1] + zeta)/20.  # easy since layers are uniform
         drhodz = (rho[:-1] - rho[1:])/dz
         Pmix[i] = g * dy * dx * (AKs*drhodz*dz).sum()
+
+        # AKs = m['AKs'][0, 1:-1, 5:-4, 5:-4].squeeze()  # vertical viscosity for tracer
+        # rho = m['rho'][0, :, 5:-4, 5:-4].squeeze()  # density
+        # zeta = m['zeta'][0,5:-4,5:-4]
+        # dz = (h[5:-4,5:-4] + zeta)/20.  # easy since layers are uniform
+        # drhodz = (rho[:-1] - rho[1:])/dz
+        # Pmix[i] = g * dy * dx * (AKs*drhodz*dz).sum()
         ##
 
         ## Calculate momentum across inlet in time (shear production)
-        AKv = m['AKv'][0, 1:-1, 5:-4, 5:-4].squeeze()  # vertical viscosity for momentum
-        u = re(m['u'][0, :, 5:-4, 5:-2],2)  # to rho grid
-        v = re(m['v'][0, :, 5:-2, 5:-4],1)  # to rho grid
+        AKv = m['AKv'][0, 1:-1, tsouth[0][0]:tnorth[0][0]+2, twest[0][2]:teast[0][2]+1].squeeze()  # vertical viscosity for momentum
+        u = re(m['u'][0, :, tsouth[0][0]:tnorth[0][0]+2, twest[0][2]:teast[0][2]+2],2)  # to rho grid
+        v = re(m['v'][0, :, tsouth[0][0]:tnorth[0][0]+3, twest[0][2]:teast[0][2]+1],1)  # to rho grid
         dudz = (u[:-1] - u[1:])/dz
         dvdz = (v[:-1] - v[1:])/dz
         Pmom[i] = rho0 * dy * dx * (AKv*(dudz**2 + dvdz**2)*dz).sum()
+        # AKv = m['AKv'][0, 1:-1, 5:-4, 5:-4].squeeze()  # vertical viscosity for momentum
+        # u = re(m['u'][0, :, 5:-4, 5:-2],2)  # to rho grid
+        # v = re(m['v'][0, :, 5:-2, 5:-4],1)  # to rho grid
+        # dudz = (u[:-1] - u[1:])/dz
+        # dvdz = (v[:-1] - v[1:])/dz
+        # Pmom[i] = rho0 * dy * dx * (AKv*(dudz**2 + dvdz**2)*dz).sum()
         ##
 
         ## Calculate power dissipation due to bottom friction, across domain
         # using velocity from bottom of water column
+        # import pdb; pdb.set_trace()
         Pfriction[i] = Cd * rho0 * dy * dx * abs((u[0]**2 + v[0]**2)**(3/2)).sum()
         ##
 
